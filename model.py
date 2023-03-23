1664d39ea04ece5b17f121032b148d7a69b7f243
YiWaiChow <jimmych679@gmail.com>
2023-03-23 16:04:30 -0400

    model structure

---
+++ model.py
@@ -0,0 +1,38 @@
+def Patch_Encoding_layer():
+    pass
+
+
+def Normalization_layer():
+    pass
+
+
+def Encoder_layer():
+    pass
+
+# modified MHA attention
+
+
+def SRA_layer():
+    pass
+
+
+def Feed_forward_layer():
+    pass
+
+
+def Spacial_Reduction_layer():
+    pass
+
+# multi head attention
+
+
+def MHA_layer():
+    pass
+
+
+def Encoder_layer():
+    pass
+
+
+def PvT():
+    pass
