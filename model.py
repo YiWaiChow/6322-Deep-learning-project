d7b659f9d97dde81b8afbcc3ec7d1e3099f66533
YiWaiChow <jimmych679@gmail.com>
2023-04-01 20:14:50 -0400

    just putting some comment to specifc the shape of the input and output

--- model.py
+++ model.py
@@ -22,14 +22,14 @@ class Patch_Embedding(nn.Module):
 
     def forward(self, x):
 
-        print(x.shape, "size of x before patch embedding, B, C, H, W")
+        print(x.shape, "size of x before patch embedding, B, C, H, W \n")
         # flatten it into 2d, so H and W collapse into number of patches, then we swap the shape
         # from [B, ED, H,W] -> [B, ED, number of patches] -> [B, number of patches, ED]
         # this is done to follow the convention of the paper, where the embedding dimension is the last dimension
         x = self.linear(x).flatten(2).transpose(1, 2)
         x = self.norm(x)
         # output shape should be [B, Number of patches, ED], where number of patches should be HW/4*2
-        print(x.shape, "size of x after patch embedding, B, Patches, ED")
+        print(x.shape, "size of x after patch embedding, B, Patches, ED \n")
         return x
 
 
@@ -40,43 +40,51 @@ class SRAttention(nn.module):
         self.head_dimension = channels/self.num_heads
         # i am not sure how to change the size here
         # self.dim = channels*self.head_dimension
+
+        # the Weight is Ci X d Head, so the input dimension should be c and the output should be d head
         self.L = nn.Linear(self.c,
                            self.head_dimension)
         self.c = channels
         self.sr = SR(height, width, channels,
                      reduction_ratio, self.c)
-        self.L2 = nn.Linear(self.c*self.head_dimension, self.c)
+        #  Wo has size Ci X Ci, this is becasuse d head = Ci/Ni, after concatnating N Ci, the dimension becomes Ci.
+        self.L2 = nn.Linear(self.c, self.c)
 
     def forward(self, query, key, value):
         SRA = None
         for i in range(self.num_heads):
+            # HW x d_head
             qi = self.L(query)
+            # HW/R^2 x d_head
             srk = self.L(self.sr(key))
+            # HW/R^2 x d_head
             srv = self.L(self.sr(value))
             # attention at stage i
+            # HW X d_head @ d_head X HW/R^2 @ HW/R^2 x d_head = > HW X d_head <--- the shape of the A_i
             Ai = (torch.softmax(qi@srk.T/(self.head_dimension**0.5)))@srv
             if(SRA is None):
                 SRA = Ai
             else:
                 SRA = torch.cat(SRA, Ai)
-
+        # SRA after concatinating should be HW X D_head*Ni -> HW X Ci
         SRA = self.L2(SRA)
-
+        print(SRA.shape, "size of x after SRA, should be HW, Ci \n ")
         return SRA
 
 
 # Spatial Reduction
 # SR(x) = Norm(Reshape(x,Ri)W^s)
 class SR(nn.Module):
-    def __init__(self, height, width, channels, reduction_ratio, dimension):
+    def __init__(self, height, width, channels, reduction_ratio):
         super().__init__()
         self.H = height
         self.W = width
         self.C = channels
         self.R = reduction_ratio
-        # self.reduction_size = self.H*self.W/(self.R**2) * (self.R**2*self.C)
-        self.linear_projection = nn.Linear(self.R**2*self.C, self.R**2*self.C)
-        self.norm = nn.LayerNorm(dimension)
+        # after reshaping x into HW/R^2 X R^2C, it takes in R^2C and projects to Ci
+        self.linear_projection = nn.Linear(self.R**2*self.C, self.C)
+        # then re layer norm on the number of channels
+        self.norm = nn.LayerNorm(self.C)
 
     def forward(self, x):
         # reduced the sptial scale of x
@@ -86,6 +94,8 @@ class SR(nn.Module):
             x, [self.H*self.W/(self.R**2), (self.R**2*self.C)])
         new_x = self.linear_projection(reduced_x)
         new_x = self.norm(new_x)
+        # output should be of size HW/R^2 x CI
+        print(new_x.shape, "size of x after SR, should be HW/R^2 x CI, CI \n")
         return new_x
 
 
