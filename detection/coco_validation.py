97408780c4d048b0da65953a8b737b840f9908ed
winstonchan1999 <71881155+winstonchan1999@users.noreply.github.com>
2023-04-16 23:55:35 -0400

    Update coco_validation.py

--- detection/coco_validation.py
+++ detection/coco_validation.py
@@ -35,10 +35,10 @@ def main(args=None):
             retinanet = retinanet.cuda()
 
     if torch.cuda.is_available():
-        retinanet.load_state_dict(torch.load(parser.model_path))
+        retinanet = torch.load(parser.model_path)
         retinanet = torch.nn.DataParallel(retinanet).cuda()
     else:
-        retinanet.load_state_dict(torch.load(parser.model_path))
+        retinanet = torch.load(parser.model_path)
         retinanet = torch.nn.DataParallel(retinanet)
 
     retinanet.training = False
